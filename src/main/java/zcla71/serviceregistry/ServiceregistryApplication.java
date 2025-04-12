37917b4b670352af2bd6c2cecf89914c941e9b60
zcla <zeclaudioaguiar@gmail.com>
2025-04-12 18:07:59 -0300

    Spring Initializr

---
+++ src/main/java/zcla71/serviceregistry/ServiceregistryApplication.java
@@ -0,0 +1,13 @@
+package zcla71.serviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceregistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceregistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zcla71/serviceregistry/ServiceregistryApplicationTests.java
@@ -0,0 +1,13 @@
+package zcla71.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceregistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
